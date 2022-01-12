d194428e92c3203439ae59c02490c7ef9753ce7e
Kelly R Cano <krcano2001@gmail.com>
2022-01-12 14:01:42 -0800

    hover effect is set

--- kelly-cano/src/components/Project.js
+++ kelly-cano/src/components/Project.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { projects } from "../projectData";
-// import { useState } from "react";
+import { useState } from "react";
 const styles = {
   // for section title
   textContainer: {},
   //  holds everying in that have to do with the project set up
   projectContainer: {},
+  section: {},
   projectA: {},
   projectImgs: {
     display: "flex",
     position: "relative",
     width: "30%",
+    // flexDirection:'row'
   },
   imageOverlay: {
     position: "absolute",
@@ -26,8 +28,6 @@ const styles = {
     alignItems: "center",
     justifyContent: "center",
     flexWrap: "wrap",
-    // opacity:'0',
-    // transition:'opacity 0.25s'
   },
   imageTag: {
     // display: "block",
@@ -37,25 +37,29 @@ const styles = {
 
   imageDescr: {
     //   lessens the space between title and descr
-    marginTop: "-4em",
+    marginTop: "-3em",
   },
-  imageTitle:{
-      color:'white',
-      textDecorationColor:'white'
-  }
+  imageTitle: {
+    color: "white",
+    textDecorationColor: "white",
+  },
 };
 const Project = () => {
-//   let [hover, setHover] = useState('false');
-// let hoverEffect={
-//    opacity:'0'
-// }
-// if(hover){
-//     hoverEffect.opacity='1'
-// }else{
-//     hoverEffect.opacity='0'
-// }
+  //   let [hover, setHover] = useState('false');
+  // let hoverEffect={
+  //    opacity:'0'
+  // }
+  // if(hover){
+  //     hoverEffect.opacity='1'
+  // }else{
+  //     hoverEffect.opacity='0'
+  // }
+  // --------------------------
+//  This state is used to create the hover effect for the Projects section
+  const [isShown, setIsShown] = useState(false);
+
   return (
-    <section id="projects">
+    <section style={styles.section} id="projects">
       <div style={styles.textContainer}>
         \<h1 style={styles.h1}>My Projects</h1>
         <h3>The following are projects I have worked on thus far:</h3>
@@ -63,29 +67,47 @@ const Project = () => {
       <div style={styles.projectContainer}>
         {projects.map((project) => (
           <a href={project.link} key={project.image} style={styles.projectA}>
-            <div style={styles.projectImgs}>
-              <img
-                alt="gallery"
-                src={project.image}
-                style={styles.imageTag}
-               
-              />
-              <div style={
-                  styles.imageOverlay
-                //   hoverEffect
-              }
-            //    onMouseEnter={(e) => {
-            //     setHover(true)
-            //   }}
-            //   onMouseLeave={(e) => {
-            //     setHover=(false)
-            //   }}
+            <div
+              style={styles.projectImgs}
+              onMouseEnter={() => setIsShown(true)}
+              onMouseLeave={() => setIsShown(false)}
             >
-               <a href={project.deployedLink}><h1 style={styles.imageTitle}>{project.title}</h1></a>
+              <img alt="gallery" src={project.image} style={styles.imageTag} />
+              {isShown && (
+                <div style={styles.imageOverlay}>
+                  <a href={project.deployedLink}>
+                    <h1 style={styles.imageTitle}>{project.title}</h1>
+                  </a>
+                  <p style={styles.imageDescr}>{project.description}</p>
+                  <a href={project.github} target="blank">
+                    <img
+                      src="./GitHub-Mark/PNG/white-github-40px.png"
+                      alt="github icon"
+                    />
+                  </a>
+                </div>
+              )}
+              {/* The following code was created prior to the hover effect */}
+              {/* <div
+                style={styles.imageOverlay}
+                //    onMouseEnter={(e) => {
+                //     setHover(true)
+                //   }}
+                //   onMouseLeave={(e) => {
+                //     setHover=(false)
+                //   }}
+              >
+                {/* <a href={project.deployedLink}>
+                  <h1 style={styles.imageTitle}>{project.title}</h1>
+                </a>
                 <p style={styles.imageDescr}>{project.description}</p>
-                <a href={project.github} target='blank'><img src="./GitHub-Mark/PNG/white-github-icon.png" alt="github icon"/></a>
-                
-              </div>
+                <a href={project.github} target="blank">
+                  <img
+                    src="./GitHub-Mark/PNG/white-github-40px.png"
+                    alt="github icon"
+                  />
+                </a> */}
+              {/* </div>  */}
             </div>
           </a>
         ))}
